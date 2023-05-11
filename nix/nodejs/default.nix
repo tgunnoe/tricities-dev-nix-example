2645d6019cf0929a019e64032c9df78195a75636
tgunnoe <taylor.gunnoe@iohk.io>
2023-05-10 23:22:34 -0400

    Remove unnecessary dir

--- nix/nodejs/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, pkgs, ... }:
-{
-  tridev-nix.method.installMethod = "symlink";
-  tridev-nix.error.nativeBuildInputs = [
-    pkgs.yarn
-  ];
-  pkg1.method.installMethod = "symlink";
-  pkg2.method.installMethod = "symlink";
-}
