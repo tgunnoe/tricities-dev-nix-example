65612a94a25e1337adafbd211a987328c5bb322d
tgunnoe <taylor.gunnoe@iohk.io>
2023-05-10 22:42:16 -0400

    Use flake-parts and nix2container

--- flake.nix
+++ flake.nix
@@ -1,24 +1,53 @@
 {
+  description = "Devshell for dapp-store backend microservices";
   inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    n2c.url = "github:nlewo/nix2container";
   };
 
-  outputs = {
-    self,
-    dream2nix,
-  } @ inp:
-    (dream2nix.lib.makeFlakeOutputs {
-      systems = ["x86_64-linux"];
-      #pkgs = pkgs;
-      config = {
-        projectRoot = ./.;
-        overridesDirs = [./nix];
-      };
-      source = ./.;
-      autoProjects = true;
+  outputs = inputs@{ self, nixpkgs, n2c, flake-parts, dream2nix }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      # Add additional systems to make output for here
+      systems = [ "x86_64-linux" ];
+      imports = [ dream2nix.flakeModuleBeta ];
+      perSystem = {inputs', self', pkgs, config, system, ...}: {
 
-    })
-    // {
-      checks = self.packages;
+        # define an input for dream2nix to generate outputs for
+        dream2nix = {
+          inputs."self" = {
+            source = inputs.self;
+            packageOverrides = {
+              tridev-nix = {
+                method.installMethod = "symlink";
+                add-yarn.nativeBuildInputs = [
+                  pkgs.yarn
+                ];
+              };
+              pkg1.method.installMethod = "symlink";
+              pkg2.method.installMethod = "symlink";
+            };
+          };
+        };
+        devShells = config.dream2nix.outputs.self.devShells;
+        packages = config.dream2nix.outputs.self.packages // {
+          oci-image-tridev-nix = inputs.n2c.packages.${system}.nix2container.buildImage {
+            name = "oci-image-tridev-nix";
+            config = {
+              entrypoint = [
+                "${pkgs.nodejs}/bin/nodejs"
+                "${self'.packages.pkg2}/lib/node_modules/pkg2/index.js"
+              ];
+            };
+          };
+        };
+      };
     };
 }
